233ece2b790218751db6963d0668a2abb082d987
Anicet Ebou <anicet.ebou@gmail.com>
2020-03-22 00:48:49 +0000

    first commit

---
+++ R/mod_loader.R
@@ -0,0 +1,120 @@
+#' loader UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd 
+#'
+#' @importFrom shiny NS tagList 
+mod_loader_ui <- function(id){
+  ns <- NS(id)
+  tagList(
+ 
+  )
+}
+    
+#' loader Server Function
+#'
+#' @noRd 
+mod_loader_server <- function(input, output, session){
+  ns <- session$ns
+ 
+}
+    
+## To be copied in the UI
+# mod_loader_ui("loader_ui_1")
+    
+## To be copied in the server
+# callModule(mod_loader_server, "loader_ui_1")
+ 
+#' loader UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd 
+#'
+#' @importFrom shiny NS tagList 
+mod_loader_ui <- function(id){
+  ns <- NS(id)
+  tagList(
+ 
+  )
+}
+    
+#' loader Server Function
+#'
+#' @noRd 
+mod_loader_server <- function(input, output, session){
+  ns <- session$ns
+ 
+}
+    
+## To be copied in the UI
+# mod_loader_ui("loader_ui_1")
+    
+## To be copied in the server
+# callModule(mod_loader_server, "loader_ui_1")
+ 
+#' loader UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd 
+#'
+#' @importFrom shiny NS tagList 
+mod_loader_ui <- function(id){
+  ns <- NS(id)
+  tagList(
+ 
+  )
+}
+    
+#' loader Server Function
+#'
+#' @noRd 
+mod_loader_server <- function(input, output, session){
+  ns <- session$ns
+ 
+}
+    
+## To be copied in the UI
+# mod_loader_ui("loader_ui_1")
+    
+## To be copied in the server
+# callModule(mod_loader_server, "loader_ui_1")
+ 
+#' loader UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd 
+#'
+#' @importFrom shiny NS tagList 
+mod_loader_ui <- function(id){
+  ns <- NS(id)
+  tagList(
+ 
+  )
+}
+    
+#' loader Server Function
+#'
+#' @noRd 
+mod_loader_server <- function(input, output, session){
+  ns <- session$ns
+ 
+}
+    
+## To be copied in the UI
+# mod_loader_ui("loader_ui_1")
+    
+## To be copied in the server
+# callModule(mod_loader_server, "loader_ui_1")
+ 
